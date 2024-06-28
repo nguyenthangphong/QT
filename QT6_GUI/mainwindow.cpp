c1f21599b6f01c613111ef7691218d01e599a07c
nthangphong2601 <nthangphong2601@gmail.com>
2024-06-29 01:51:08 +0700

    Updated for Signals and Slots

--- QT6_GUI/mainwindow.cpp
+++ QT6_GUI/mainwindow.cpp
@@ -1,14 +1,34 @@
 #include "mainwindow.h"
-#include "./ui_mainwindow.h"
+#include "ui_mainwindow.h"
 
-MainWindow::MainWindow(QWidget *parent)
-    : QMainWindow(parent)
-    , ui(new Ui::MainWindow)
+MainWindow::MainWindow(QWidget *parent) : QMainWindow(parent), ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    /*
+        String Notation
+        connect(ui->myButton, SIGNAL(clicked()), this, SLOT(change_text()));
+    */
+
+    /*
+        Functor Notation : Regular Slots
+        connect(ui->myButton, &QPushButton::clicked, this, &MainWindow::change_text);
+    */
+
+    /*
+        Functor Notation : Regular Slots
+    */
+
+    connect(ui->myButton, &QPushButton::clicked, [=](){
+        ui->myLabel->setText("Some other text from lambda");
+    });
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+void MainWindow::change_text()
+{
+    ui->myLabel->setText("Some other text");
 }
--- QT6_GUI/mainwindow.h
+++ QT6_GUI/mainwindow.h
@@ -4,9 +4,7 @@
 #include <QMainWindow>
 
 QT_BEGIN_NAMESPACE
-namespace Ui {
-class MainWindow;
-}
+namespace Ui { class MainWindow; }
 QT_END_NAMESPACE
 
 class MainWindow : public QMainWindow
@@ -16,6 +14,9 @@ class MainWindow : public QMainWindow
 public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
+
+private slots:
+    void change_text();
 
 private:
     Ui::MainWindow *ui;

c32b8973b728dad838926bfab1091a4110cb49f9
nthangphong2601 <nthangphong2601@gmail.com>
2024-06-29 08:30:58 +0700

    Updated code for Slider and ProgressBar connected

--- QT6_GUI/mainwindow.cpp
+++ QT6_GUI/mainwindow.cpp
@@ -1,14 +1,38 @@
 #include "mainwindow.h"
-#include "./ui_mainwindow.h"
+#include "ui_mainwindow.h"
+#include <QDebug>
 
-MainWindow::MainWindow(QWidget *parent)
-    : QMainWindow(parent)
-    , ui(new Ui::MainWindow)
+MainWindow::MainWindow(QWidget *parent) : QMainWindow(parent), ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    /*
+        String Notation
+        connect(ui->horizontalSlider, SIGNAL(valueChanged(int)), ui->progressBar, SLOT(setValue(int)));
+        connect(ui->horizontalSlider, SIGNAL(valueChanged(int)), this, SLOT(respond(int)));
+    */
+
+    /*
+        Functor Notation
+        connect(ui->horizontalSlider, &QSlider::valueChanged, ui->progressBar, &QProgressBar::setValue);
+        connect(ui->horizontalSlider, &QSlider::valueChanged, this, &MainWindow::respond);
+    */
+
+    /*
+        Functor Notation : Lambas
+    */
+
+    connect(ui->horizontalSlider, &QSlider::valueChanged, [=](){
+        ui->progressBar->setValue(ui->horizontalSlider->value());
+        respond(ui->horizontalSlider->value());
+    });
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+void MainWindow::respond(int value)
+{
+    qDebug() << "value : " << value;
 }
--- QT6_GUI/mainwindow.h
+++ QT6_GUI/mainwindow.h
@@ -17,6 +17,9 @@ public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
+private slots:
+    void respond(int value);
+
 private:
     Ui::MainWindow *ui;
 };

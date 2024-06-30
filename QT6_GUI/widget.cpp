a8ce79f47d6582091edba3b6fe53600d231bf0e5
nthangphong2601 <nthangphong2601@gmail.com>
2024-06-30 14:48:29 +0700

    QLabel

--- QT6_GUI/widget.cpp
+++ QT6_GUI/widget.cpp
@@ -1,8 +1,16 @@
 #include "widget.h"
+#include "ui_widget.h"
+#include <QPixmap>
 
-Widget::Widget(QWidget *parent) : QWidget{parent}
+Widget::Widget(QWidget *parent) : QWidget{parent}, ui(new Ui::Widget)
 {
+    ui->setupUi(this);
+    ui->imageTextLabel->move(100, 30);
 
+    QPixmap my_image(":/images/Kobie_Mainoo.png");
+
+    ui->imageLabel->move(0, 70);
+    ui->imageLabel->setPixmap(my_image.scaled(400, 400));
 }
 
 Widget::~Widget()
--- QT6_GUI/widget.h
+++ QT6_GUI/widget.h
@@ -3,6 +3,10 @@
 
 #include <QWidget>
 
+QT_BEGIN_NAMESPACE
+namespace Ui { class Widget; }
+QT_END_NAMESPACE
+
 class Widget : public QWidget
 {
     Q_OBJECT
@@ -10,5 +14,7 @@ public:
     Widget(QWidget *parent = nullptr);
     ~Widget();
 
+private:
+    Ui::Widget *ui;
 };
 #endif // WIDGET_H

f2e0eb086731c6e426e4cd6737e5b42495c9f58d
nthangphong2601 <nthangphong2601@gmail.com>
2024-06-30 08:56:28 +0700

    QPushButton

---
+++ QT6_GUI/widget.cpp
@@ -0,0 +1,35 @@
+#include "widget.h"
+#include <QPushButton>
+#include <QDebug>
+
+Widget::Widget(QWidget *parent) : QWidget{parent}
+{
+    // Clicked Signal
+    QFont buttonFont("Times", 20, QFont::Bold);
+
+    QPushButton *button = new QPushButton("Button1", this);
+    button->setMinimumSize(200, 100);
+    button->setFont(buttonFont);
+
+    connect(button, &QPushButton::clicked, [=](){
+        qDebug() << "Button clicked";
+    });
+
+    QPushButton *button2 = new QPushButton("Button2", this);
+    button2->setMinimumSize(200, 100);
+    button2->move(205, 0);
+    button2->setFont(buttonFont);
+
+    connect(button2, &QPushButton::clicked, [=](){
+        qDebug() << "Button clicked";
+    });
+
+    connect(button2, &QPushButton::released, [=](){
+        qDebug() << "Button released";
+    });
+}
+
+Widget::~Widget()
+{
+
+}
